086f9eb63792e66262d1c2146646521e4b16a6e0
Brutus5000 <Brutus5000@gmx.net>
2021-11-25 21:07:50 +0100

    Add mongodb and nodebb

---
+++ scripts/k8s-helpers.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+# Helper functions for interacting with a k8s cluster
+
+# fail on errors
+set -e
+
+# contains(string, substring)
+#
+# Returns 0 if the specified string contains the specified substring,
+# otherwise returns 1.
+contains() {
+  string="$1"
+  substring="$2"
+  if test "${string#*$substring}" != "$string"
+  then
+    return 0  # $substring is in $string
+  else
+    return 1  # $substring is not in $string
+  fi
+}
+
+# check_cluster()
+#
+# Returns 0 if the current kubectl cluster is faf-k8s or otherwise if it is manually confirmed
+# otherwise returns 1
+check_cluster() {
+  echo -n "You are using kubectl from: "
+  which kubectl
+  cluster=$(kubectl config current-context)
+
+  if [ "$cluster" != "faf-k8s" ]
+  then
+    echo "Please make sure your kubectl context is set to faf."
+    echo -n "Confirm [y/n]: "
+    read -r confirm
+    if [ "$confirm" != 'y' ]; then exit 1; fi;
+  fi
+}
+
+check_resource_exists_or_fail() {
+  RESOURCE_TYPE=$1
+  RESOURCE_NAME=$2
+
+  EXISTS=$(kubectl get "$RESOURCE_TYPE" "$RESOURCE_NAME" >/dev/null 2>/dev/null && echo true || echo false)
+  if test "$EXISTS" = "false"
+  then
+    echo "$RESOURCE_TYPE $RESOURCE_NAME not applied. Aborting."
+    exit 1
+  fi
+}
+
+get_pod_variable() {
+  POD=$1
+  VARIABLE="\${$2}"
+  kubectl exec $POD -- /bin/sh -c "echo ${VARIABLE}"
+}
+
+# has_annotation(resourceType [e.g. pod], resourceName [e.g. mongodb], annotationName, annotationValue)
+#
+# Returns 0 if the annotation is present with the required value for the k8s resource
+# otherwise returns 1.
+has_annotation_with_value() {
+  RESOURCE_TYPE=$1
+  RESOURCE_NAME=$2
+  ANNOTATION_NAME=$3
+  REQUIRED_VALUE=$4
+
+  ACTUAL_VALUE=$(kubectl get "$RESOURCE_TYPE" "$RESOURCE_NAME" -o json | jq -r ".metadata.annotations.$ANNOTATION_NAME")
+
+  if test "$REQUIRED_VALUE" = "$ACTUAL_VALUE"
+  then
+    return 0
+  else
+    return 1
+  fi
+}
